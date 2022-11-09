207ad930ae5e97c80e3c214d08bade423d8e905a
e1920614 <115053688+Bast975@users.noreply.github.com>
2022-11-09 12:43:38 -0400

    Home update

--- Bastien_CAMBRAY/src/Components/Header.js
+++ Bastien_CAMBRAY/src/Components/Header.js
@@ -5,10 +5,9 @@ import "./Header.css";
 
 function Header() {
   return <div>
-    <h3 class="float-md-start mb-0"> Bastien Cambray </h3>
+    <h3 class="float-md-start mb-0"> Bastien CAMBRAY </h3>
     <nav class="nav nav-masthead justify-content-center float-md-end">
-      <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#"> Home </a>
-      <a class="nav-link fw-bold py-1 px-0" href="#"> Bio </a>
+      <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#"> Bio </a>
       <a class="nav-link fw-bold py-1 px-0" href="#"> CV </a>
       <a class="nav-link fw-bold py-1 px-0" href="#"> Work </a>
       <a class="nav-link fw-bold py-1 px-0" href="#"> Blog </a>
